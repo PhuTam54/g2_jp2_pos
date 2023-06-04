02702c6a5197d3995f7ec491755d3c661e2a59a0
PhuTam54 <phutamytb@gmail.com>
2023-06-04 22:53:17 +0700

    Tạo khung chương trình.

---
+++ src/src/Main.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static Stage mainStage;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        mainStage = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("views/pos.fxml"));
+        primaryStage.setScene(new Scene(root,600,400));
+        primaryStage.setTitle("POS");
+        primaryStage.show();
+
+    }
+}
